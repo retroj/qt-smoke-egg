ae498dc6919fd8d3e9ecf7527afd66c8516c8ccb
John Foerch <jjfoerch@earthlink.net>
2013-02-11 19:13:43 -0500

    examples/hello: style, structure

--- examples/hello.scm
+++ examples/hello.scm
@@ -18,9 +18,10 @@
 (let ((qtgui (make <SchemeSmokeBinding> 'smoke qtgui-smoke))
       (stack (make-stack 3))
       (classid #f)
-      (methid #f)
-      (qapp #f)
-      (widget #f))
+      (methid #f))
+
+  ;; print info about QApplication constructor
+  ;;
   (let ((classid (find-class qtgui "QApplication"))
         (methid (find-method qtgui "QApplication" "QApplication$?")))
     (printf "QApplication classId: [~A, ~A], QApplication($?) methId: [~A, ~A]~%"
@@ -28,50 +29,56 @@
             (ModuleIndex-index classid)
             (smoke-modulename (ModuleIndex-smoke methid))
             (ModuleIndex-index methid)))
+
+  ;; print info about QWidget constructor
+  ;;
+  (let ((classid (find-class qtgui "QWidget"))
+        (methid (find-method qtgui "QWidget" "QWidget")))
+    (printf "QWidget classId: [~A, ~A], QWidget() methId: [~A, ~A]~%"
+            (smoke-modulename (ModuleIndex-smoke classid))
+            (ModuleIndex-index classid)
+            (smoke-modulename (ModuleIndex-smoke methid))
+            (ModuleIndex-index methid)))
+
+  ;; make application and widget objects.  QApplication takes two args,
+  ;; pointers to argc and argv.
+  ;;
   (stack-set-int-pointer! stack 1 0)     ;; &argc
   (stack-set-unsigned-long! stack 2 0)   ;; argv
-  (set! qapp (instantiate qtgui "QApplication" "QApplication$?" stack))
 
-  ;; create a widget
-  ;;
-  (set! classid (find-class qtgui "QWidget"))
-  (set! methid (find-method qtgui "QWidget" "QWidget"))
-  (printf "QWidget classId: [~A, ~A], QWidget() methId: [~A, ~A]~%"
-          (smoke-modulename (ModuleIndex-smoke classid))
-          (ModuleIndex-index classid)
-          (smoke-modulename (ModuleIndex-smoke methid))
-          (ModuleIndex-index methid))
-  (set! widget (instantiate qtgui "QWidget" "QWidget" stack))
+  (let ((qapp (instantiate qtgui "QApplication" "QApplication$?" stack))
+        (widget (instantiate qtgui "QWidget" "QWidget" stack)))
 
-  ;; show the widget
-  ;;
-  (set! methid (find-method qtgui "QWidget" "show"))
-  (printf "QWidget classId: [~A, ~A], show() methId: [~A, ~A]~%"
-          (smoke-modulename (ModuleIndex-smoke classid))
-          (ModuleIndex-index classid)
-          (smoke-modulename (ModuleIndex-smoke methid))
-          (ModuleIndex-index methid))
-  (call-method qtgui methid widget stack)
+    ;; show the widget
+    ;;
+    (set! classid (find-class qtgui "QWidget"))
+    (set! methid (find-method qtgui "QWidget" "show"))
+    (printf "QWidget classId: [~A, ~A], show() methId: [~A, ~A]~%"
+            (smoke-modulename (ModuleIndex-smoke classid))
+            (ModuleIndex-index classid)
+            (smoke-modulename (ModuleIndex-smoke methid))
+            (ModuleIndex-index methid))
+    (call-method qtgui methid widget stack)
 
-  ;; QApplication exec
-  ;;
-  (set! classid (find-class qtgui "QApplication"))
-  (set! methid (find-method qtgui "QApplication" "exec"))
-  (printf "QApplication classId: ~A, exec() methId: [~A, ~A]~%"
-          (ModuleIndex-index classid)
-          (smoke-modulename (ModuleIndex-smoke methid))
-          (ModuleIndex-index methid))
-  (call-method-with-callbacks qtgui methid #f stack)
-  (printf "QApplication exec() return value: ~A~%"
-          (stack-int stack 0))
+    ;; QApplication exec
+    ;;
+    (set! classid (find-class qtgui "QApplication"))
+    (set! methid (find-method qtgui "QApplication" "exec"))
+    (printf "QApplication classId: ~A, exec() methId: [~A, ~A]~%"
+            (ModuleIndex-index classid)
+            (smoke-modulename (ModuleIndex-smoke methid))
+            (ModuleIndex-index methid))
+    (call-method-with-callbacks qtgui methid #f stack)
+    (printf "QApplication exec() return value: ~A~%"
+            (stack-int stack 0))
 
-  ;; destroy the QApplication instance
-  ;;
-  (set! methid (find-method qtgui "QApplication" "~QApplication"))
-  (printf "QApplication classId: ~A, ~~QApplication() methId: [~A, ~A]~%"
-          (ModuleIndex-index classid)
-          (smoke-modulename (ModuleIndex-smoke methid))
-          (ModuleIndex-index methid))
-  (call-method-with-callbacks qtgui methid qapp stack))
+    ;; destroy the QApplication instance
+    ;;
+    (set! methid (find-method qtgui "QApplication" "~QApplication"))
+    (printf "QApplication classId: ~A, ~~QApplication() methId: [~A, ~A]~%"
+            (ModuleIndex-index classid)
+            (smoke-modulename (ModuleIndex-smoke methid))
+            (ModuleIndex-index methid))
+    (call-method-with-callbacks qtgui methid qapp stack)))
 (delete-qtcore-smoke)
 (delete-qtgui-smoke)
